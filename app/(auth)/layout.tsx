e0193b994146be23dc7228dad5749242e60de469
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-08-26 02:37:08 +0300

    ADDING clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React from "react";
+import '../globals.css';
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads App!",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return;
+  <ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        {children}
+        </body>
+    </html>
+  </ClerkProvider>;
+}
