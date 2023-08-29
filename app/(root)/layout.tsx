a2a1eb4b5d32e20d01c4ef38fefcfac5ade6a12f
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-08-29 18:59:37 +0300

    FINISHED Project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
+import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Bottombar from "@/components/shared/Bottombar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Topbar from "@/components/shared/Topbar";
-import { dark } from "@clerk/themes";
-import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,15 +28,16 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <html lang="en">
+      <html lang='en'>
         <body className={inter.className}>
           <Topbar />
 
-          <main className="flex flex-row">
+          <main className='flex flex-row'>
             <LeftSidebar />
-            <section className="main-container">
-              <div className="w-full max-w-4xl">{children}</div>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,18 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
+let isConnected = false; // Variable to track the connection status
 
-let isConnected = false; // var to check if mongoose is connected
+export const connectToDB = async () => {
+  // Set strict query mode for Mongoose to prevent unknown field queries.
+  mongoose.set("strictQuery", true);
 
-export const connectToDB = async ()=>{
-    mongoose.set('strictQuery', true);
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
 
-    if(!process.env.MONGODB_URL) return console.log('MONGODB_URL Not found!');
-    if (isConnected) return console.log('Mongoose is Already Connected');
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        isConnected = true ;
-        console.log(`Connected Successfully to mongoose`);
-    }catch (error){
-            console.log(error);
-    }
-}
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log("MongoDB connection already established");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true; // Set the connection status to true
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
